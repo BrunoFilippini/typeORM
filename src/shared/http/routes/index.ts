0bea788816afa276e93f25db4cf6e07da43485eb
Bruno Filippini <bruno.filippini@alicerceedu.com.br>
2023-01-14 15:41:32 -0300

    product routers

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello dev !' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;
